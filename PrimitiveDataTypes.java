a00ba34999a8e800d24c960b82c36be69bf69c20
alansam23 <alansamuel0023@gmail.com>
2023-05-16 21:39:46 +0530

    Added: PrimitiveDataTypes Output

---
+++ PrimitiveDataTypes.java
@@ -0,0 +1,20 @@
+public class PrimitiveDataTypes {
+    public static void main(String[] args) {
+        int a = 5;
+        float b = 5.99f;
+        char c = '0';
+        boolean d = true;
+        byte e = 100;
+        short g =1000;
+        long h = 10000000000L;
+        double i = 12456587;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(g);
+        System.out.println(h);
+        System.out.println(i);
+    }
+}
